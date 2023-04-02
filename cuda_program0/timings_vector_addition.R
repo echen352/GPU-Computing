5cb8cb5a0489affc0fcf18291a41bde355368a32
echen352 <ellischen352@gmail.com>
2023-04-01 20:26:11 -0700

    removed vector addition program

--- cuda_program0/timings_vector_addition.R
+++
@@ -1,9 +0,0 @@
-data <- read_csv("timings.csv")
-data$newSize <- as.factor(data$size)
-p <- data %>%
-  ggplot(aes(newSize, time, color = cuda)) +
-  geom_point() +
-  ylab("time (sec) log scale") +
-  xlab("Vector Size") +
-  labs(title = "Vector Addition Cuda vs Non-Cuda Performance")
-p + scale_y_log10()
